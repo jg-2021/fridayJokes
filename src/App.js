8edeba6696ea95dd9a8e7ad6e9b2e59901fb0043
Joshua Garner <jgarner1989@yahoo.com>
2021-03-05 13:04:58 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import React from 'react'
 import Jokes from './components/Jokes'
-import {BrowserRouter as Router, Route} from "react-router-dom"
+// import {BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css'
 
 function App() {
